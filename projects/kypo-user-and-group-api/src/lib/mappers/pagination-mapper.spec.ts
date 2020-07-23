f1a75ebd1ca91477c46925f2a3bf8f4b2aa9bde8
Martin Hamerník <445720@mail.muni.cz>
2020-07-23 12:32:02 +0000

    Resolve "Use cypress image in CI"

---
+++ projects/kypo-user-and-group-api/src/lib/mappers/pagination-mapper.spec.ts
@@ -0,0 +1,21 @@
+import { PaginationMapper } from './pagination-mapper';
+import { PaginationDTO } from '../DTO/pagination-dto.model';
+
+describe('PaginationMapper', () => {
+  it('should map correctly', () => {
+    const dto: PaginationDTO = {
+      number: 1,
+      number_of_elements: 5,
+      size: 10,
+      total_elements: 5,
+      total_pages: 20,
+    };
+
+    const pagination = PaginationMapper.mapDTOToPagination(dto);
+    expect(pagination.page).toEqual(dto.number);
+    expect(pagination.numberOfElements).toEqual(dto.number_of_elements);
+    expect(pagination.size).toEqual(dto.size);
+    expect(pagination.totalElements).toEqual(dto.total_elements);
+    expect(pagination.totalPages).toEqual(dto.total_pages);
+  });
+});
